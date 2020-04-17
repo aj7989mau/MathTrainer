8e7c0562673803f97055445e931da32c1a9e06b0
Niklas Hultin <60153534+aj7989mau@users.noreply.github.com>
2020-04-17 11:19:48 +0200

    Revert "Merge pull request #24 from aj7989mau/newbranch"
    This reverts commit 88509ebfff9a70c4043480880a4cdf196f228256, reversing
    changes made to a9135c06f8f19d49463c1305a83195eca8053a82.

--- MathTrainerClient/src/controllers/LogInController.java
+++ MathTrainerClient/src/controllers/LogInController.java
@@ -22,12 +22,9 @@ public class LogInController extends SceneControllerParent {
         }
     }
 
-
     public void logInClicked(ActionEvent actionEvent) {
             //ToDo: Kod för att logga in med befintlig användare
         mainController.setScene(ScenesEnum.Home);
-
-
     }
 
     public void newUserClicked(ActionEvent actionEvent) {
