62d1ace1472b9dc69d8a2b6ca9df8b150a4dbc36
aj8457mau <62740785+aj8457mau@users.noreply.github.com>
2020-04-23 03:14:17 +0200

    Updated the GUI of client
    GUI update

--- MathTrainerClient/src/controllers/ExercisesController.java
+++ MathTrainerClient/src/controllers/ExercisesController.java
@@ -2,4 +2,13 @@ package controllers;
 
 public class ExercisesController extends MainMenuControllerParent {
     //ToDO: Metoder för att hantera de actions användaren kan göra i Övningar.
+
+
+
+//ArrayList list = new ArrayList
+
+    //list.add(fråga)
+
+
+
 }
