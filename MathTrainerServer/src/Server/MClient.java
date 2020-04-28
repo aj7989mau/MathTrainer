dc76bf6b95b1b282d96a973866f19b8e81162284
Johanna <johanna.dahlborn@gmail.com>
2020-04-28 16:09:22 +0200

    Login request
    some changes

--- MathTrainerServer/src/Server/MClient.java
+++ MathTrainerServer/src/Server/MClient.java
@@ -17,7 +17,7 @@ public class MClient {
         this.port = port;
         keepRunning = true;
 
-        try {
+        try {¥
             //Connecting to server
             System.out.println("Connecting to " + serverIp + " on port " + port);
             connection = new Socket(serverIp, port);
