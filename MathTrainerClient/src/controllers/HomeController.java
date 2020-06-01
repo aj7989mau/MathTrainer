26f410688844ca61648447487deb4deb167c0c0b
Niklas Hultin <60153534+aj7989mau@users.noreply.github.com>
2020-06-01 16:33:53 +0200

    Trophies
    Added logic for displaying logic when max score is reached. Also added more constraints on user name (less than 20 characters, no special characters)

--- MathTrainerClient/src/controllers/HomeController.java
+++ MathTrainerClient/src/controllers/HomeController.java
@@ -3,6 +3,7 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
 import sharedEntities.User;
 
 /**
@@ -25,6 +26,16 @@ public class HomeController extends MainMenuControllerParent implements Initiali
     ProgressBar pbCounting;
     @FXML
     ProgressBar pbPlaceholder;
+    @FXML
+    ImageView countingTrophy;
+    @FXML
+    ImageView statisticsTrophy;
+    @FXML
+    ImageView placeholderTrophy;
+    @FXML
+    ImageView geometryTrophy;
+    @FXML
+    ImageView totalTrophy;
 
     private User user;
     private int[] results;
@@ -36,19 +47,53 @@ public class HomeController extends MainMenuControllerParent implements Initiali
             welcomeLabel.setText("Välkommen " + user.getUserName());
             results = user.getResults();
 
-            int total = 0;
-            for (int result : results){
-                total += result;
-            }
-
-            pbCounting.setProgress((double) results[0]/10);
-            pbStatistics.setProgress((double) results[1]/10);
-            pbPlaceholder.setProgress((double) results[2]/10);
-            pbGeometry.setProgress((double) results[3]/10);
-            pbAllCategories.setProgress((double) total/40);
+            setProgress();
 
         } else {
             welcomeLabel.setText("Välkommen ");
+        }
+    }
+
+    public void setProgress(){
+        int total = 0;
+        for (int result : results){
+            total += result;
+        }
+        pbCounting.setProgress((double) results[0]/10);
+        pbStatistics.setProgress((double) results[1]/10);
+        pbPlaceholder.setProgress((double) results[2]/10);
+        pbGeometry.setProgress((double) results[3]/10);
+        pbAllCategories.setProgress((double) total/40);
+
+        if (results[0] == 10){
+            countingTrophy.setVisible(true);
+        } else {
+            countingTrophy.setVisible(false);
+        }
+
+        if (results[1] == 10){
+            statisticsTrophy.setVisible(true);
+        } else {
+            statisticsTrophy.setVisible(false);
+        }
+
+        if (results[2] == 10){
+            placeholderTrophy.setVisible(true);
+        } else {
+            placeholderTrophy.setVisible(false);
+        }
+
+        if (results[3] == 10){
+            geometryTrophy.setVisible(true);
+        } else {
+            geometryTrophy.setVisible(false);
+        }
+
+
+        if (total == 40){
+            totalTrophy.setVisible(true);
+        } else {
+            totalTrophy.setVisible(false);
         }
     }
 }
--- MathTrainerClient/src/controllers/NewUserController.java
+++ MathTrainerClient/src/controllers/NewUserController.java
@@ -9,6 +9,9 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Controller for handling button-presses in the scene NewUser.fxml. Each method represent a possible user action.
  * @author Niklas Hultin
@@ -36,10 +39,15 @@ public class NewUserController extends SceneControllerParent implements Initiali
      * @param actionEvent
      */
     public void createUserClicked(ActionEvent actionEvent) {
-        if (!password.getText().equals(passwordRepeat.getText())) {
+        String regex = "^[a-zA-Z0-9]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(username.getText());
+        if (username.getText().length() < 5 || username.getText().length() > 20) {
+            mainController.popUpWindow(Alert.AlertType.ERROR, "Användarnamnet är för kort eller långt", "Användarnamnet måste vara mellan 5 och 20 tecken långt");
+        } else if(!matcher.matches()){
+            mainController.popUpWindow(Alert.AlertType.ERROR, "Specialtecken är ej tillåtna", "Användarnamnet får ej innehålla specialtecken, som t ex !, &, *");
+        } else if (!password.getText().equals(passwordRepeat.getText())) {
             mainController.popUpWindow(Alert.AlertType.ERROR, "Felaktigt lösenord", "Lösenorden du angav stämmer ej överens med varandra");
-        } else if (username.getText().length() < 5) {
-            mainController.popUpWindow(Alert.AlertType.ERROR, "Användarnamnet är för kort", "Användarnamnet måste vara minst 5 tecken långt");
         } else if (password.getText().length() < 6){
             mainController.popUpWindow(Alert.AlertType.ERROR, "Lösenordet är för kort", "Lösenordet måste vara minst 6 tecken långt");
         } else {
